2559310f2e197c5210847ff2668bb01b1945881b
Earl <earl.willis@publicismodem.com>
2015-06-29 16:35:44 -0400

    cart

--- pepto/src/test/java/brand/dawn/DawnAddCartDesktop.java
+++ pepto/src/test/java/brand/dawn/DawnAddCartDesktop.java
@@ -293,7 +293,7 @@ public class DawnAddCartDesktop {
 		        System.out.println("Image Name " +name);
 		        takeScreenPortrait(name);
 		        driver.manage().timeouts().implicitlyWait(450000, TimeUnit.SECONDS);
-		        driver.get("http://int.author.cascadeclean.pgsitecore.com/en-us/shopping-cart");
+		          driver.get("http://int.author.dawn.pgsitecore.com/en-us/shopping-cart");
 			     driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
